dd854bb021751ef576fba2ed4859a8f5eb9f39ad
Jeffrey Andersen <jeffreykandersen@gmail.com>
2020-06-17 22:31:04 -0700

    added form submission functionality

--- survey/views.py
+++ survey/views.py
@@ -5,6 +5,14 @@ def index(request):
     return render(request, "index.html")
 
 def result(request):
+    return render(request, "result.html")
+
+def process(request):
     if request.method =="POST":
-        return render (request, "result.html")
-    return redirect (request, "index.html")
+        request.session ['name'] = request.POST['name']
+        request.session ['location'] = request.POST['location']
+        request.session ['language'] = request.POST['language']
+        request.session ['comment'] = request.POST['comment']
+        return redirect('/result')
+    return redirect('/')
+
